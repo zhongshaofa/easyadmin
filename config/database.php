c8eb99b873404fb97862ed62c90bd628323d9dc9
zhongshaofa <2286732552@qq.com>
2018-11-08 12:44:04 +0800

    更新 database.php

--- config/database.php
+++ config/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => 'data.99php.cn',
+    'hostname'        => 'localhost',
     // 数据库名
-    'database'        => 'test_99php_cn',
+    'database'        => '99blog',
     // 用户名
-    'username'        => 'test_99php_cn',
+    'username'        => 'root',
     // 密码
-    'password'        => 'YY2rKNafcF8dHWfj',
+    'password'        => 'root',
     // 端口
     'hostport'        => '',
     // 连接dsn
