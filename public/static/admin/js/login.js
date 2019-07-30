d1b4006ff351fb2d67d1c49b4921c1e3ee2f73e6
Mr.Chung <2286732552@qq.com>
2019-07-30 12:45:38 +0800

    修改登录样式

--- public/static/admin/js/login.js
+++ public/static/admin/js/login.js
@@ -1,27 +1,36 @@
-layui.extend({
-    tool: "plugs/layuimini/layuimini-tool",
-});
-layui.define(['form', 'tool'], function (exports) {
-    var $ = layui.jquery,
-        form = layui.form,
-        tool = layui.tool;
+require(["jquery", "jquery-particleground"], function ($, undefined) {
 
-    var controller = new function () {
+    layui.extend({
+        tool: "plugs/layuimini/layuimini-tool",
+    }).define(['form', 'tool'], function (exports) {
+        var $ = layui.jquery,
+            form = layui.form,
+            tool = layui.tool;
 
-        this.index = function () {
-            if (top.location != self.location) top.location = self.location;
-            form.on('submit(login)', function (data) {
-                data = data.field;
-                tool.request.post("/admin/login/index", data, function (res) {
-                    tool.msg.success(res.msg, function () {
-                        window.location.href = res.url;
+        var controller = new function () {
+
+            this.index = function () {
+                if (top.location != self.location) top.location = self.location;
+                $(document).ready(function(){
+                    $('#container').particleground({
+                        dotColor:'#5cbdaa',
+                        lineColor:'#5cbdaa'
                     });
                 });
-                return false;
-            });
+                form.on('submit(login)', function (data) {
+                    data = data.field;
+                    tool.request.post("/admin/login/index", data, function (res) {
+                        tool.msg.success(res.msg, function () {
+                            window.location.href = res.url;
+                        });
+                    });
+                    return false;
+                });
+            };
+
         };
 
-    };
+        exports("controller", controller);
+    });
 
-    exports("controller", controller);
 });
--- public/static/config.js
+++ public/static/config.js
@@ -5,5 +5,7 @@ require.config({
         "layui": ["plugs/layui-v2.5.4/layui"],
         "echarts": ["plugs/echarts/echarts.min"],
         "echarts-theme": ["plugs/echarts/echarts-theme"],
+        "jquery": ["plugs/jquery-3.4.1/jquery-3.4.1.min"],
+        "jquery-particleground": ["plugs/jq-module/jquery.particleground.min"],
     }
 });
