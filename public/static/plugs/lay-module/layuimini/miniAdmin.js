c4f74ecda2d9dfbcfe2b9478477990171e903a27
Mr.Chung <2286732552@qq.com>
2020-05-09 00:28:41 +0800

    修复缩放菜单

--- public/static/plugs/lay-module/layuimini/miniAdmin.js
+++ public/static/plugs/lay-module/layuimini/miniAdmin.js
@@ -9,7 +9,8 @@
 define(["jquery", "miniMenu", "miniTheme", "miniTab"], function ($, miniMenu, miniTheme, miniTab) {
 
     var $ = layui.$,
-        layer = layui.layer;
+        layer = layui.layer,
+        element = layui.element;
 
     if (!/http(s*):\/\//.test(location.href)) {
         var tips = "请先将项目部署至web容器（Apache/Tomcat/Nginx/IIS/等），否则部分数据将无法显示";
