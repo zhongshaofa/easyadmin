1df5bc10a842796f4feebb8145f654cd74da84ae
chung <chung@99php.cn>
2020-06-12 15:41:14 +0800

    更新layui版本为2.5.6

--- public/static/config-admin.js
+++ public/static/config-admin.js
@@ -9,7 +9,7 @@ require.config({
         "echarts": ["plugs/echarts/echarts.min"],
         "echarts-theme": ["plugs/echarts/echarts-theme"],
         "easy-admin": ["plugs/easy-admin/easy-admin"],
-        "layuiall": ["plugs/layui-v2.5.5/layui.all"],
+        "layuiall": ["plugs/layui-v2.5.6/layui.all"],
         "layui": ["plugs/layui-v2.5.5/layui"],
         "miniAdmin": ["plugs/lay-module/layuimini/miniAdmin"],
         "miniMenu": ["plugs/lay-module/layuimini/miniMenu"],
