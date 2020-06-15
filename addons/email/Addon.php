8410258e8b474fa3879dfa44a237ac5d751ce9b4
chung <chung@99php.cn>
2020-06-15 17:28:00 +0800

    修改

---
+++ addons/email/Addon.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace addons\sms;
+
+
+class Addon
+{
+
+    /**
+     * 安装方法
+     */
+    public function install(){
+
+    }
+
+    /**
+     * 卸载方法
+     */
+    public function uninstall(){
+
+    }
+
+    public function send(){
+
+    }
+
+}
--- vendor/zhongshaofa/easy-addons/src/Service.php
+++ vendor/zhongshaofa/easy-addons/src/Service.php
@@ -16,6 +16,6 @@ class Service extends BaseService
 {
     public function register()
     {
-
+        die;
     }
 }
