a91b7e7435ddbb698cd72a7203aa786b64b0cdf9
Mr.Chung <2286732552@qq.com>
2020-06-24 01:39:02 +0800

    新增插件描述

---
+++ addons/answer/Plugin.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace addons\answer;
+
+
+/**
+ * 插件公共方法
+ * Class Addon
+ * @package addons\email
+ */
+class Plugin
+{
+
+    /**
+     * 安装方法
+     */
+    public function install()
+    {
+        return true;
+    }
+
+    /**
+     * 卸载方法
+     */
+    public function uninstall()
+    {
+        return true;
+    }
+
+}
