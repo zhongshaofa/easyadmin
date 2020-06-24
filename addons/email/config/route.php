6639d21f3456e299731944f433ec91f6a6b2ee93
chung <chung@99php.cn>
2020-06-24 09:16:39 +0800

    删除多余文件

--- addons/email/config/route.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-
-    // 插件模块是否开启强制路由
-    'url_route_must' => false,
-
-];
--- addons/email/middleware.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-
-    // 检测用户是否登录
-    \app\admin\middleware\CheckAdmin::class,
-
-];
