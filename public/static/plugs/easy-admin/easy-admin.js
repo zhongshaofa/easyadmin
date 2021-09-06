0dc9b8fa63c1e0c93c39881cf4675bea610bb77f
cshaptx4869 <994774638@qq.com>
2021-09-06 14:16:00 +0800

    Add: 增加文件上传类型的控制

--- public/static/plugs/easy-admin/easy-admin.js
+++ public/static/plugs/easy-admin/easy-admin.js
@@ -1283,24 +1283,23 @@ define(["jquery", "tableSelect", "ckeditor"], function ($, tableSelect, undefine
 
                 if (uploadList.length > 0) {
                     $.each(uploadList, function (i, v) {
-                        var exts = $(this).attr('data-upload-exts'),
+                        var uploadExts = $(this).attr('data-upload-exts') || init.upload_exts,
                             uploadName = $(this).attr('data-upload'),
-                            uploadNumber = $(this).attr('data-upload-number'),
-                            uploadSign = $(this).attr('data-upload-sign');
-                        exts = exts || init.upload_exts;
-                        uploadNumber = uploadNumber || 'one';
-                        uploadSign = uploadSign || '|';
-                        var elem = "input[name='" + uploadName + "']",
+                            uploadNumber = $(this).attr('data-upload-number') || 'one',
+                            uploadSign = $(this).attr('data-upload-sign') || '|',
+                            uploadAccept = $(this).attr('data-upload-accept') || 'file',
+                            uploadAcceptMime = $(this).attr('data-upload-mimetype') || '',
+                            elem = "input[name='" + uploadName + "']",
                             uploadElem = this;
 
                         // 监听上传事件
                         upload.render({
                             elem: this,
                             url: admin.url(init.upload_url),
-                            accept: 'file',
-                            exts: exts,
-                            // 让多图上传模式下支持多选操作
-                            multiple: (uploadNumber !== 'one') ? true : false,
+                            exts: uploadExts,
+                            accept: uploadAccept,//指定允许上传时校验的文件类型
+                            acceptMime: uploadAcceptMime,//规定打开文件选择框时，筛选出的文件类型
+                            multiple: uploadNumber !== 'one',//是否多文件上传
                             done: function (res) {
                                 if (res.code === 1) {
                                     var url = res.data.url;
@@ -1324,8 +1323,7 @@ define(["jquery", "tableSelect", "ckeditor"], function ($, tableSelect, undefine
                         $(elem).bind("input propertychange", function (event) {
                             var urlString = $(this).val(),
                                 urlArray = urlString.split(uploadSign),
-                                uploadIcon = $(uploadElem).attr('data-upload-icon');
-                            uploadIcon = uploadIcon || "file";
+                                uploadIcon = $(uploadElem).attr('data-upload-icon') || "file";
 
                             $('#bing-' + uploadName).remove();
                             if (urlString.length > 0) {
@@ -1370,13 +1368,10 @@ define(["jquery", "tableSelect", "ckeditor"], function ($, tableSelect, undefine
 
                 if (uploadSelectList.length > 0) {
                     $.each(uploadSelectList, function (i, v) {
-                        var exts = $(this).attr('data-upload-exts'),
-                            uploadName = $(this).attr('data-upload-select'),
-                            uploadNumber = $(this).attr('data-upload-number'),
-                            uploadSign = $(this).attr('data-upload-sign');
-                        exts = exts || init.upload_exts;
-                        uploadNumber = uploadNumber || 'one';
-                        uploadSign = uploadSign || '|';
+                        var uploadName = $(this).attr('data-upload-select'),
+                            uploadNumber = $(this).attr('data-upload-number') || 'one',
+                            uploadSign = $(this).attr('data-upload-sign') || '|';
+
                         var selectCheck = uploadNumber === 'one' ? 'radio' : 'checkbox';
                         var elem = "input[name='" + uploadName + "']",
                             uploadElem = $(this).attr('id');
